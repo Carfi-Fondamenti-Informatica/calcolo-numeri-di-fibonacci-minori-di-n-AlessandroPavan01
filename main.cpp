d076ad4455d600cc6a41af72dfabbf19fd10907a
AlessandroPavan01 <91598666+AlessandroPavan01@users.noreply.github.com>
2021-11-14 14:59:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0, a=0, b=1, c=a+b;
+    cin>> n;
+    cout<< b <<endl;
+
+    for (int i=2; i<(n+1); i++){
+        if(c<=n){
+            cout<< c <<endl;
+            a = b;
+            b = c;
+            c = a+b;
+        }
+    }
+    return 0;
 }
